e71d62588d1268ba72535a89087700ba5b87a0a1
Micah <micahnyoung95@gmail.com>
2022-03-24 21:12:12 -0500

    completed exercises without Bonus Missions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -18,4 +18,21 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  memberBeingEdited: object = null;
+  add(crewMember: string, isFirstMission: boolean){
+    this.crew.push({name: crewMember, firstMission: isFirstMission})
+  }
+  remove(memberBeingRemoved: object) {
+    let index: number = this.crew.indexOf(memberBeingRemoved)
+    this.crew.splice(index, 1)
+  }
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+    console.log(this.crew[0])
+    }
+    
 }
